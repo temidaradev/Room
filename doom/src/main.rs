ef3b0516d1d7fd38f2193e9b9a027d196d8acae9
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-07-10 14:19:03 +0300

    test clone

--- doom/src/main.rs
+++ doom/src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Clone)]
+struct Player {
+    x: f32,
+    y: f32,
+    health: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut player = Player {
+        x: 0.0,
+        y: 0.0,
+        health: 100,
+    };
+
+    print_player(&player);
+    move_player(&mut player, 10.0, 5.0);
+    let other_player = player;
+    let cloned_player = other_player.clone();
+
+    print_player(&cloned_player);
+}
+
+fn print_player(p: &Player) {
+    println!("Player at ({}, {}) with {} health", p.x, p.y, p.health);
+}
+
+fn move_player(p: &mut Player, dx: f32, dy: f32) {
+    p.x += dx;
+    p.y += dy;
 }
