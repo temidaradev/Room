ee4805eb58a872bb7fad8bd55a2d48068d090957
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-07-15 21:54:33 +0300

    lets cook

--- doom/src/main.rs
+++ doom/src/main.rs
@@ -43,7 +43,6 @@ fn main() {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
