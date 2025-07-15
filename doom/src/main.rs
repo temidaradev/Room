319d8b5df1975d2e7af340dd811b7f7ccca91544
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-07-15 23:13:17 +0300

    test splitting sdl handler

--- doom/src/main.rs
+++ doom/src/main.rs
@@ -1,7 +1,4 @@
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use std::time::Duration;
+use engine::*;
 
 use std::fs::File;
 use std::io::BufReader;
@@ -13,40 +10,8 @@ fn main() {
         Err(e) => println!("Error: {}", e),
     }
 
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    Engine::draw_testing();
 
-    let window = video_subsystem
-        .window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-    'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-
-        canvas.present();
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
 }
 
 fn read_wad(filename: &str) -> Result<(), Box<dyn std::error::Error>> {
